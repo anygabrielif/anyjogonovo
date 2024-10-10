9ee29c86c8cea490314b51bc36d0b448b8f22eac
anyjogonovo <anygabrielifa@gmail.com>
2024-10-10 09:03:28 -0300

    any

---
+++ anyjogonovo/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace anyjogonovo;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ anyjogonovo/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿namespace anyjogonovo;
+
+public partial class MainPage : ContentPage
+{
+
+	const int Gravidade = 20;
+
+	const int TempoEntreFrames = 1000;
+
+	
+
+
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+		count++;
+
+		if (count == 1)
+			CounterBtn.Text = $"Clicked {count} time";
+		else
+			CounterBtn.Text = $"Clicked {count} times";
+
+		SemanticScreenReader.Announce(CounterBtn.Text);
+	}
+}
+
