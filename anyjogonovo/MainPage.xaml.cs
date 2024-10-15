0dd76eb5b8fe39f5761a7bef98f30077bb2a2c57
any <anygabrielifa@gmail.com>
2024-10-15 09:59:18 -0300

    anyjogonovo

--- anyjogonovo/MainPage.xaml.cs
+++ anyjogonovo/MainPage.xaml.cs
@@ -3,30 +3,13 @@
 public partial class MainPage : ContentPage
 {
 
-	const int Gravidade = 20;
-
-	const int TempoEntreFrames = 1000;
-
-	
-
-
-	int count = 0;
-
 	public MainPage()
 	{
 		InitializeComponent();
 	}
 
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
-
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
-
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
+     private void BotaoIniciar(object sender, EventArgs args)
+  {
+      Application.Current.MainPage = new jogoPage();
+  }
 }
-
---
+++ anyjogonovo/jogoPage.xaml.cs
@@ -0,0 +1,35 @@
+namespace anyjogonovo;
+
+public partial class JogoPage : ContentPage
+{
+	const int gravidade = 30;
+	const int tempoEntreFrames = 25;
+	bool estaMorto = false;
+    private object fotopassaro;
+
+    public JogoPage()
+	{
+		InitializeComponent();
+	}
+	void AplicaGravidade()
+	{
+		imgpassaro.TranslationY+=gravidade;
+	}
+
+	async Task Desenhar()
+	{
+		while (!estaMorto)
+		{
+			AplicaGravidade();
+			await Task.Delay(tempoEntreFrames);
+		}
+	}
+	protected override void OnAppearing()
+	{
+		base.OnAppearing();
+		Desenhar();
+	}
+
+
+
+}
